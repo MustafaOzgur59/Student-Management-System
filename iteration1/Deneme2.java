5142a3342d60f37931e6b62b807dbf11fcb372b2
Mozgur <dfaunm@gmail.com>
2022-11-02 14:14:40 +0300

    moved code

--- iteration1/Deneme2.java
+++
@@ -1,7 +0,0 @@
-package iteration1;
-
-public class Deneme2 {
-    public static void main(String[] args) {
-        System.out.println("Deneme2");
-    }
-}
---
+++ iteration1/Hello.java
@@ -0,0 +1,7 @@
+package iteration1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("iteration1.Hello World");
+    }
+}
