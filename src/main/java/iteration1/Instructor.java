6c3ed61c9be4d4e4cfab7ce8ce1bc5220f5e6187
O-Bugra <osmanbugrag@gmail.com>
2022-11-10 19:50:24 +0300

    added Instructor class and debug FacultyMember class

---
+++ src/main/java/iteration1/Instructor.java
@@ -0,0 +1,31 @@
+package iteration1;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Instructor extends FacultyMember {
+
+	
+	private ArrayList<Course> coursesOfferedList;
+
+	public Instructor(String name, String id) {
+		super(name, id);
+	}
+	
+	public void gradeStudents(Student student, Course course) {
+		
+		Random rand = new Random();
+		float int_random = (float)rand.nextInt(9)/2;
+		student.getCurrentSemester().getCourses().add(course.getCourseCode(),int_random);	
+	}
+
+	public ArrayList<Course> getCoursesOfferedList() {
+		return coursesOfferedList;
+	}
+
+	public void setCoursesOfferedList(ArrayList<Course> coursesOfferedList) {
+		this.coursesOfferedList = coursesOfferedList;
+	}
+
+	
+	
+}
