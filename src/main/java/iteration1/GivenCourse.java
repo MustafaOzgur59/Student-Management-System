4514198db2fa5a47e493fb8197b90d5d9d8f38d4
ecekizilkaya <ecekizilkaya1688@gmail.com>
2022-11-13 23:54:55 +0300

    GivenCourse class added.

--- src/main/java/iteration1/GivenCourse.java
+++ src/main/java/iteration1/GivenCourse.java
@@ -7,6 +7,8 @@ public class GivenCourse {
     private String courseCode;
     @JsonProperty("grade")
     private float grade;
+    @JsonProperty("letterGrade")
+    private String letterGrade;
 
     private Integer credit;
 
@@ -14,6 +16,7 @@ public class GivenCourse {
         this.courseCode = courseCode;
         this.grade = grade;
         this.credit = credit;
+        calculateLetterGrade();
     }
 
     public GivenCourse() {
@@ -37,6 +40,42 @@ public class GivenCourse {
 
     public Integer getCredit() {
         return credit;
+    }
+
+    public String getLetterGrade() {
+        return letterGrade;
+    }
+
+    public void setLetterGrade(String letterGrade) {
+        this.letterGrade = letterGrade;
+    }
+
+    public void setCredit(Integer credit) {
+        this.credit = credit;
+    }
+
+    public void calculateLetterGrade() {
+
+            Float aFloat = this.grade;
+            if (aFloat == 0) {
+                this.letterGrade = "FF";
+            } else if (aFloat == 0.5) {
+                this.letterGrade = "FD";
+            } else if (aFloat == 1) {
+                this.letterGrade = "DD";
+            } else if (aFloat == 1.5) {
+                this.letterGrade = "DC";
+            } else if (aFloat == 2) {
+                this.letterGrade = "CC";
+            } else if (aFloat == 2.5) {
+                this.letterGrade = "CB";
+            } else if (aFloat == 3) {
+                this.letterGrade = "BB";
+            } else if (aFloat == 3.5) {
+                this.letterGrade = "BA";
+            } else if (aFloat == 4) {
+                this.letterGrade = "AA";
+            }
     }
 
     @Override
