3ed37e2ceaf0be7fe9e9e45b9a40506b8dd94997
Engin-Bilici <enginbilici2000@gmail.com>
2022-12-01 19:51:09 +0300

    Curriculum class has been changed.

--- src/main/java/iteration2/Curriculum.java
+++ src/main/java/iteration2/Curriculum.java
@@ -1,22 +1,29 @@
-package iteration2;
-
-import iteration1.Course;
+package iteration1;
 
 import java.util.ArrayList;
 
 public class Curriculum {
-    private final ArrayList<iteration1.Course>[] COURSES ;
+    private final ArrayList<Course>[] COURSES ;
 
     public Curriculum() {
-       COURSES = new ArrayList[8];
-       for(int i=0; i<8;i++){
-           COURSES[i] = new ArrayList<>();
-       }
+        COURSES = new ArrayList[8];
+        for(int i=0; i<8;i++){
+            COURSES[i] = new ArrayList<>();
+        }
     }
 
     public ArrayList<Course>[] getCOURSES() {
         return COURSES;
     }
 
-
+    public Course getCourse(String courseName){
+        for (ArrayList<Course> courses : COURSES){
+            for (Course c : courses){
+                if (c.getName().equals(courseName)){
+                    return c;
+                }
+            }
+        }
+        return null;
+    }
 }
