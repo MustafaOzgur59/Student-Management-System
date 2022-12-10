9eed6635b4236e2a6fd2d90f2975b54425b264fd
ecekizilkaya <117164329+ecekizilkaya@users.noreply.github.com>
2022-12-10 21:05:05 +0300

    FacultyMember has been changed.

--- src/main/java/iteration2/FacultyMember.java
+++ src/main/java/iteration2/FacultyMember.java
@@ -5,35 +5,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import iteration2.Course;
 import iteration2.Student;
 
-public abstract class FacultyMember {
+public abstract class FacultyMember extends Person {
 	
 	
-	@JsonProperty("name")
-	private String name ;
-	private String id ;
+
 
 	public FacultyMember(String name, String id) {
-		this.name = name;
-		this.id = id;
-		
+		super(id,name);
 	}
 
 	public abstract void gradeStudents(Student student, Course course);
-	
-	public String getName() {
-		return name;
+
+	@Override
+	public String toString() {
+		return "FacultyMember{} " + super.toString();
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
 }
