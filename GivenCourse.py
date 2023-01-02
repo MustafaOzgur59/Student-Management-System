1642453bdcc2ee881629a96051089431cfb0430e
MustafaOzgur59 <mustafaozgurh@gmail.com>
2023-01-02 23:01:09 +0300

    Added given course class

---
+++ GivenCourse.py
@@ -0,0 +1,33 @@
+class GivenCourse:
+
+    def __init__(self, courseCode, grade, credit):
+        self.courseCode = courseCode
+        self.grade = grade
+        self.credit = credit
+        self.letterGrade = ""
+
+    def getLetterGrade(self):
+        return self.letterGrade
+
+    def setLetterGrade(self, letterGrade):
+        self.letterGrade = letterGrade
+
+    def calculateLetterGrade(self):
+        if self.grade == 0:
+            self.letterGrade = "FF"
+        elif self.grade == 0.5:
+            self.letterGrade = "FD"
+        elif self.grade == 1:
+            self.letterGrade = "DD"
+        elif self.grade == 1.5:
+            self.letterGrade = "DC"
+        elif self.grade == 2:
+            self.letterGrade = "CC"
+        elif self.grade == 2.5:
+            self.letterGrade = "CB"
+        elif self.grade == 3:
+            self.letterGrade = "BB"
+        elif self.grade == 3.5:
+            self.letterGrade = "BA"
+        else:
+            self.letterGrade = "AA"
